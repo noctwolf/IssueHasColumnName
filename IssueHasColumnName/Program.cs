cf8629094ac93151d0c42900a13c2b06f278a760
noctwolf <noctwolf@gmail.com>
2019-08-17 21:20:18 +0800

    add sql

--- IssueHasColumnName/Program.cs
+++ IssueHasColumnName/Program.cs
@@ -8,9 +8,13 @@ namespace IssueHasColumnName
         static void Main(string[] args)
         {
             using var context = new TableSplittingContext();
+
             var id = context.DetailedOrders.Select(f => f.Id).FirstOrDefault();
+            //sql SELECT TOP(1) [o].[IdFoo] FROM[Orders] AS[o]
             Console.WriteLine(id);
+
             var name = context.DetailedOrders.Select(f => f.Name).FirstOrDefault();
+            //sql SELECT TOP(1) [o].[Name] FROM [Orders] AS [o]
             Console.WriteLine(name);
         }
     }
